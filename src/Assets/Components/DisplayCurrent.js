b2ad01be7369c03956d457a9eb1b74132d06aab4
Kasador <huntershaw23@gmail.com>
2019-07-22 17:45:20 -0500

    UPDATED: added fahrenheit temp.

--- src/Assets/Components/DisplayCurrent.js
+++ src/Assets/Components/DisplayCurrent.js
@@ -5,7 +5,11 @@ const DisplayCurrent = ({ info }) =>
     <div className="DisplayCurrent">
         <div className="DisplayWrapper">
             <h1>{info.name}</h1>
-            {info.wind.speed}
+            <h2>{info.weather[0].description}</h2>
+            <div className="Temp">
+                <img src={`http://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`} alt="icons" />
+                <h1>{Math.floor((info.main.temp - 273.15) * 9/5 + 32)}&#8457;</h1>
+            </div>
         </div>
     </div>
 : null;
