10cca8e9f826ad880dd386655877105647e5f751
Kasador <huntershaw23@gmail.com>
2019-07-22 02:40:44 -0500

    ADDED: dynamic background based on day/night.

--- src/Assets/Main/App.js
+++ src/Assets/Main/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../Components/Nav';
 
 function App() {
+    // change background based on day/night
+    let date = new Date();
+    let currentTime = date.getHours();
+
+    setInterval(() => {
+        currentTime = date.getHours();
+        console.log(`15 Minute Update: ${currentTime}`);
+    }, 900000);
+    // useEffect to render out correct background image
+    useEffect(() => {
+        if (currentTime > 0) {
+            document.body.style.backgroundImage = 'url(' + require('../Images/night.jpg') + ')';
+        } else if (currentTime >= 16) {
+            document.body.style.backgroundImage = 'url(' + require('../Images/night.jpg') + ')';
+        } else if (currentTime <= 7) {
+            document.body.style.backgroundImage = 'url(' + require('../Images/day.jpg') + ')';
+        }
+    },[]);
+
     return (
         <div className="App">
             <Nav />
